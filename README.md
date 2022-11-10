78a750509b63ade57c22b183e090a9c8827d2a42
EllAchE <26192612+EllAchE@users.noreply.github.com>
2022-11-10 00:01:02 -0800

    delete comments and add readme

---
+++ README.md
@@ -0,0 +1,1 @@
+Dataset of words pulled from kaggle dataset https://www.kaggle.com/datasets/rtatman/english-word-frequency?resource=download
--- Trie.py
+++ Trie.py
@@ -23,17 +23,3 @@ class Trie:
                 node = new_node
 
         node.is_word = True
-
-    # def dfs(self, node, prefix):
-    #     """Depth-first traversal of the trie
-    #
-    #     Args:
-    #         - node: the node to start with
-    #         - prefix: the current prefix, for tracing a
-    #             word while traversing the trie
-    #     """
-    #     if node.is_end:
-    #         self.output.append((prefix + node.char))
-    #
-    #     for child in node.children.values():
-    #         self.dfs(child, prefix + node.char)
